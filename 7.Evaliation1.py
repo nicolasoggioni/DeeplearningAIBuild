f32530ea0880d7cf2d4ab2fb95a422bd310178d3
Nicolas Oggioni <n.oggioni@sap.com>
2024-02-26 17:02:45 -0500

    Create 7.Evaliation1.py

---
+++ 7.Evaliation1.py
@@ -0,0 +1,20 @@
+import os
+
+### import utils
+from openai import OpenAI
+
+client = OpenAI(
+        api_key=os.getenv('OPENAI_API_KEY')
+    )
+
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+    
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=temperature,
+    )
+    
+    return response.choices[0].message.content
+
+###Exercise Instructions
