469dc69b6cf171109f507ba7fe393488a8c0f0b0
Nicolas Oggioni <n.oggioni@sap.com>
2024-04-16 16:02:19 -0400

    Commit

--- 8.PizzaExample.py
+++ 8.PizzaExample.py
@@ -1,9 +1,6 @@
 import os
 
 from openai import OpenAI
-# import tiktoken
-#from dotenv import load_dotenv, find_dotenv
-# _ = load_dotenv(find_dotenv()) # read local .env file
 
 client = OpenAI(
         api_key=os.getenv('OPENAI_API_KEY')
